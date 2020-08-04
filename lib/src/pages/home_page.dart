d8d065cf8fe13dfd8e049e1c6dd5766b06e3f2b7
mikeangeloo <mikeangeloo87@outlook.com>
2020-08-04 08:02:34 -0500

    agregando navegación simple

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/providers/menu_provider.dart';
 import 'package:componentes/src/utils/icono_string_util.dart';
 import 'package:flutter/material.dart';
@@ -24,13 +25,13 @@ class HomePage extends StatelessWidget {
       initialData: [],
       builder: (context, AsyncSnapshot<List<dynamic>> shapshot) {
         return ListView(
-          children: _listaItems(shapshot.data),
+          children: _listaItems(shapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _listaItems(List<dynamic> data) {
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
     final List<Widget> opciones = [];
 
     data.forEach((opt) {
@@ -38,7 +39,10 @@ class HomePage extends StatelessWidget {
         title: Text(opt['texto']),
         leading: getIcon(opt['icon']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
-        onTap: () {},
+        onTap: () {
+          final route = MaterialPageRoute(builder: (context) => AlertPage());
+          Navigator.push(context, route);
+        },
       );
 
       opciones..add(widgetTemp)..add(Divider());
